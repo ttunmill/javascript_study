651c4dc03a3872de9ff82f8473ab53ac7b1ff29e
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-04 17:19:25 +0900

    24/06/04 javascript

--- project/casper/script/script.js
+++ project/casper/script/script.js
@@ -3,7 +3,10 @@ const sub_bg = document.querySelector(".sub_bg")
 const nav = document.querySelector("nav")
 const nav_clone = nav.cloneNode(true) //nav복제 변수
 const m_nav = document.querySelector(".m_nav")
-console.log(sub, sub_bg, nav, nav_clone, m_nav) //복제대상 붙여넣기 부모
+const m_gnb_li = nav_clone.querySelectorAll(" .gnb > li")
+const m_gnb_sub = nav_clone.querySelectorAll(" .gnb > li > .sub")
+//console.log(sub, sub_bg, nav, nav_clone, m_nav, m_gnb_li, m_gnb_sub) //복제대상 붙여넣기 부모
+console.log(m_gnb_li, m_gnb_sub) //복제대상 붙여넣기 부모
 
 //모든 서브, 서브배경 숨기기
 for(var i of sub) {i.style.display = "none"}
@@ -16,4 +19,13 @@ m_nav.appendChild(nav_clone)
 nav_clone.style.display = "none"
 m_nav.addEventListener("click", () => {
     nav_clone.style.display = "block"
+})
+
+
+for(var i of m_gnb_sub) {i.style.display = "none"}
+m_gnb_li.forEach(function(index, number) {
+    m_gnb_li[number].addEventListener("click", () => {
+        for(var i of m_gnb_sub) {i.style.display = "none"}
+        m_gnb_sub[number].style.display = "block"
+    })
 })
